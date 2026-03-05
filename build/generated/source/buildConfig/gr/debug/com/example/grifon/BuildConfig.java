3dbbc431d61d981b3289de47e123ce5b38f3b2ab
Grifon <grifons@gmail.com>
2026-03-06 01:18:42 +0200

    build

--- build/generated/source/buildConfig/gr/debug/com/example/grifon/BuildConfig.java
+++ build/generated/source/buildConfig/gr/debug/com/example/grifon/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.grifon.eshop";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "gr";
-  public static final int VERSION_CODE = 2;
-  public static final String VERSION_NAME = "1.1.0";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "1.2.0";
   // Gateway base URL
   public static final String API_BASE_URL = "http://10.0.2.2:3000/";
   // Gateway shop identifier
